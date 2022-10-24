169394299c851e9c657cfee2a3f36763cc16c92c
sergioizc <sergio.zuniga1901@alumnos.ubiobio.cl>
2022-10-24 13:54:02 -0300

    avances

--- Modelo/Agente.java
+++ Modelo/Agente.java
@@ -1,7 +1,9 @@
 package Modelo;
 public class Agente {
+    Entorno objEntorno = new Entorno();
     private String objetivo;
     private int velocidad;
+    String rutas;
 
     public Agente(String obj, int vel) {
         this.objetivo = obj;
@@ -20,4 +22,52 @@ public class Agente {
     public int getVelocidad() {
         return velocidad;
     }
+    
+    public String whatIF(){
+        String rutasx ="perdido";
+        if(objEntorno.buscaR(1, objetivo)){
+            rutasx="T1";        
+        }
+        else 
+        if(objEntorno.buscaR(2, objetivo)){
+            rutasx="T2";        
+        }
+        else 
+        if(objEntorno.buscaR(3, objetivo)){
+            rutasx="T3";        
+        }
+        else 
+        if(objEntorno.buscaR(4, objetivo)){
+            rutasx="T4";        
+        }
+
+        return rutasx;
+    }
+     public double actualizarEstado(String estadop, String accion, double percepcion){
+        double distancia=0;
+        System.out.println("conduciendo por "+estadop+" la acción: "+accion+", percepción(tiempo): "+percepcion);
+        distancia = objEntorno.distancia(percepcion, velocidad);
+        return distancia;
+     }
+     public boolean accion(double distancia){
+        System.out.println("conduciendo hacia "+objetivo+" por "+ rutas);
+        if((distancia>=objEntorno.getDT1()) && (objetivo.equals(objEntorno.getObj1()))){
+            System.out.println("Llegón al Destino");
+            return false;
+        }if((distancia>=objEntorno.getDT1()) && (objetivo.equals(objEntorno.getObj2()))){
+            System.out.println("Llegón al Destino");
+            return false;
+        }
+        if((distancia>=objEntorno.getDT2()) && (objetivo.equals(objEntorno.getObj3()))){
+            System.out.println("Llegón al Destino");
+            return false;
+        }if((distancia>=objEntorno.getDT3()) && (objetivo.equals(objEntorno.getObj4()))){
+            System.out.println("Llegón al Destino");
+            return false;
+        }if((distancia>=objEntorno.getDT4()) && (objetivo.equals(objEntorno.getObj5()))){
+            System.out.println("Llegón al Destino");
+            return false;
+        }
+        return true;
+     }
 }
--- Modelo/Entorno.java
+++ Modelo/Entorno.java
@@ -1,4 +1,70 @@
 package Modelo;
 public class Entorno {
-    
+    public static final String getT1 = null;
+    String []objetivos={ "A","B","C","D","E"};
+    String []T1={objetivos[0],objetivos[1],"80"};
+    String []T2={objetivos[2],"10"};
+    String []T3={objetivos[3],"70"};
+    String []T4={objetivos[4],"30"};
+
+    public double distancia(double t, double v){
+        return (v*t)/3600;
+    }
+    public boolean buscaRuta(String T[], String buscando){
+        for(int i = 0;i<T.length;i++){
+            if(T[i].equals(buscando)){
+                return(true);
+            }
+        }
+        return (false);
+    }
+    public boolean buscaR (int ruta, String buscado){
+        boolean encontrado = false;
+        switch(ruta){
+            case 1:
+                encontrado = buscaRuta(T1,buscado);
+                break;
+
+            case 2:
+                encontrado = buscaRuta(T2,buscado);
+                break;
+
+            case 3:
+                encontrado = buscaRuta(T3, buscado);
+                break;
+
+            case 4:
+                encontrado = buscaRuta(T4,buscado);
+                break;
+        }
+        return encontrado;
+    }
+    public double getDT1(){
+        return Double.parseDouble(T1[2]);
+    }
+    public double getDT2(){
+        return Double.parseDouble(T2[1]);
+    }
+    public double getDT3(){
+        return Double.parseDouble(T3[1]);
+    }
+    public double getDT4(){
+        return Double.parseDouble(T4[1]);
+    }
+    public String getObj1(){
+        return (objetivos[0]);
+    }
+    public String getObj2(){
+        return (objetivos[1]);
+    }
+    public String getObj3(){
+        return (objetivos[2]);
+    }
+    public String getObj4(){
+        return (objetivos[3]);
+    }
+    public String getObj5(){
+        return(objetivos[4]);
+    }
 }
+
